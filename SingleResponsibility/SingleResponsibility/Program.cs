a9a3c443a4df863b40320ae4c0381c41cc368431
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-09-30 13:33:50 -0500

    20200930: Single Responsibility and refactor

---
+++ SingleResponsibility/SingleResponsibility/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SingleResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CEmployee cEmployee = new CEmployee("cristian", "enginner", 19, 1000);
+            Console.WriteLine(cEmployee.CI());
+
+        }
+    }
+}
---
+++ SingleResponsibilityRefactor/SingleResponsibilityRefactor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SingleResponsibilityRefactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CEmployee cEmployee = new CEmployee("cristian", "enginner", 19, 1000);
+            Console.WriteLine(cEmployee);
+            Console.WriteLine(CTaxesDepartment.CI(cEmployee));
+            CTaxesDepartment.PI(cEmployee);
+        }
+    }
+}
+
