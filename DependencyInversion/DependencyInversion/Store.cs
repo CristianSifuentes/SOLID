06936b94865575ee2bee36aa5895a97f8ed4c1d3
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-10-08 12:26:57 -0500

    20201008: DependencyInversion

---
+++ DependencyInversion/DependencyInversion/Store.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInversion
+{
+    class Store
+    {
+        private List<Product> inventory;
+
+        public List<Product> Inventory { get => inventory; set => inventory = value; }
+
+        public Store()
+        {
+            inventory = new List<Product>();
+        }
+
+        public void AddProduct(Product pProduct)
+        {
+            inventory.Add(pProduct);
+            Console.WriteLine("Add {0}", pProduct.Nombre);
+        }
+    }
+}
---
+++ DependencyInversionRefactor/DependencyInversionRefactor/Store.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInversionRefactor
+{
+    class Store : IAuditable
+    {
+        private List<Product> inventory;
+
+        public List<Product> Inventory { get => inventory; set => inventory = value; }
+
+        public Store()
+        {
+            inventory = new List<Product>();
+        }
+
+        public void AddProduct(Product pProduct)
+        {
+            inventory.Add(pProduct);
+            Console.WriteLine("Add {0}", pProduct.Nombre);
+        }
+
+        public IEnumerable<Product> GetProducts(int pTipo)
+        {
+            List<Product> encontrados = new List<Product>();
+            foreach(Product p in inventory)
+            {
+                if (p.Tipo == pTipo)
+                    encontrados.Add(p);
+            }
+            return encontrados;
+        }
+    }
+}
